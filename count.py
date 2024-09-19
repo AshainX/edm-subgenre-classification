8bd8bfa15d77b514423965f841fd156140724fd0
AshainX <swain.ashutosh18@gmail.com>
2024-09-19 20:22:30 +0530

    changed the output style

--- count.py
+++ count.py
@@ -9,8 +9,8 @@
 #     return folder_file_count
 
 # # Specify the path to your folder
-# folder_path = r'C:\Users\ashut\Desktop\TRAIN'
-# file_counts = count_files_in_directory(folder_path)
+# TEST = r'C:\Users\ashut\Desktop\TRAIN'
+# file_counts = count_files_in_directory(TEST)
 
 # # Print the results
 # for folder, count in file_counts.items():
@@ -19,24 +19,43 @@
 
 import os
 
-def count_files_in_directory(directory):
-    folder_file_count = {}
+def countFilesInDirectory(directory):
+    folderFileCount = {}
     
     for root, dirs, files in os.walk(directory):
-        folder_file_count[root] = len(files)
+        folderFileCount[root] = len(files)
         
         # Ensure to include subdirectories even if they have no files
         for dir in dirs:
-            subfolder_path = os.path.join(root, dir)
-            if subfolder_path not in folder_file_count:
-                folder_file_count[subfolder_path] = 0
+            subfolderPath = os.path.join(root, dir)
+            if subfolderPath not in folderFileCount:
+                folderFileCount[subfolderPath] = 0
 
-    return folder_file_count
+    return folderFileCount
+#for checking empty folders
+# def printEmptyFolders(fileCounts):
+#     print("\nFolders with 0 files:")
+#     for folder, count in sorted(fileCounts.items()):
+#         if count == 0:
+#             print(f'Empty Folder: {folder}')
+
+def printFileCounts(fileCounts):
+    print("\nFolder and File Count:")
+    for folder, count in sorted(fileCounts.items()):
+        print(f'Folder: {folder} - Number of files: {count}')
 
 # Specify the path to your folder
-folder_path = r'C:\Users\ashut\Desktop\TRAIN'
-file_counts = count_files_in_directory(folder_path)
+TRAIN = r'C:\Users\ashut\Desktop\TRAIN'
+TEST = r'C:\Users\ashut\Desktop\TEST'
 
-# Print the results
-for folder, count in sorted(file_counts.items()):
-    print(f'Folder: {folder} - Number of files: {count}')
+fileCountTEST = countFilesInDirectory(TEST)
+fileCountTRAIN = countFilesInDirectory(TRAIN)
+
+# printing the results
+print("TEST Directory Files:")
+printFileCounts(fileCountTEST)
+# printEmptyFolders(fileCountTEST)
+
+print("\nTRAIN Directory Files:")
+printFileCounts(fileCountTRAIN)
+# printEmptyFolders(fileCountTRAIN)
