d92a7d5d61d632933e69d528c2ddfbe0ea71cdeb
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2024-10-10 11:53:27 +0530

    added melspectogram analysis

---
+++ melspectimgexractor.py
@@ -0,0 +1,9 @@
+'''
+
+Components of a Mel Spectrogram:
+
+	•	X-axis (Time): Represents the progression of time.
+	•	Y-axis (Frequency in Mel scale): Represents the different frequencies in Mel scale, from low to high.
+	•	Color Intensity: Represents the amplitude or energy at each frequency band at a given time. Brighter or darker regions indicate stronger or weaker amplitudes.
+
+'''
